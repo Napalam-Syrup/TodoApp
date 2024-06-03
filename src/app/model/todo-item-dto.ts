94f694e4d0a110f14a0e7cde1db524c6c4a6a46b
benjaminneoh <benjaminneoh2928@gmail.com>
2024-06-03 22:59:42 +1000

    comment on service layer

--- src/app/model/todo-item-dto.ts
+++ src/app/model/todo-item-dto.ts
@@ -1,5 +1,12 @@
-export interface TodoItemDTO {
+export interface TodoItemDTO { //if this is a new todo to be add to database i will name something "newTodoItemDTO"
     title: string;
     isDone: boolean;
     description: string;
+}
+
+//Example
+export interface NewTodoItemDTO {
+  title: string;
+  isDone: boolean;
+  description: string;
 }
